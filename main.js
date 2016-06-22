e7dc6c0b15b2bd2d86595c770c1f0e3d1f1c1e66
Caryn Ligon <carynligon@gmail.com>
2016-06-22 14:49:24 -0500

    javascript working

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ document.querySelector('button').onclick = function () {
   var number1 = document.querySelector('.number1').value;
   var number2 = document.querySelector('.number2').value;
   var answer = Number(number1) + Number(number2);
+  document.querySelector('.output').innerHTML = answer;
   console.log(answer);
 }
