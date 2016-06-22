741db685b6952a92f0566521a0bdb02b8c5e1c4e
Caryn Ligon <carynligon@gmail.com>
2016-06-22 15:57:26 -0500

    started working on if/else statement for typeof requirement

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 document.querySelector('button').onclick = function () {
   var number1 = document.querySelector('.number1').value;
   var number2 = document.querySelector('.number2').value;
+
+  if (typeof number1 != 'number' || typeof number2 != 'number') {
+    window.alert(Please type only numbers);
+  }
+
+  else {
   var answer = Number(number1) + Number(number2);
   document.querySelector('.output').innerHTML = answer;
   console.log(answer);
+}
 }
