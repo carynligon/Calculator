d151a95a4bbbb088c3c4e50b193b372f950cd8dc
Caryn Ligon <carynligon@gmail.com>
2016-06-22 22:24:20 -0500

    no errors, still not working properly though

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 document.querySelector('button').onclick = function () {
   var number1 = document.querySelector('.number1').value;
   var number2 = document.querySelector('.number2').value;
-
-  if (typeof number1 != 'number' || typeof number2 != 'number') {
-    window.alert(Please type only numbers);
-  }
-
-  else {
   var answer = Number(number1) + Number(number2);
   document.querySelector('.output').innerHTML = answer;
   console.log(answer);
-}
+};
+
+  if (typeof number1 != 'number' || typeof number2 != 'number') {
+    confirm('Please enter only numbers');
+  }
+
+  else {
+  document.querySelector('button').onclick ();
 }
