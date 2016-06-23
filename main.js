8119b06f452803fe312b5714ce9ca59e62d4369c
Caryn Ligon <carynligon@gmail.com>
2016-06-22 22:50:12 -0500

    isNaN function working properly

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 document.querySelector('button').onclick = function () {
   var number1 = document.querySelector('.number1').value;
   var number2 = document.querySelector('.number2').value;
-  var answer = Number(number1) + Number(number2);
-  document.querySelector('.output').innerHTML = answer;
-  console.log(answer);
-};
 
-  if (typeof number1 != 'number' || typeof number2 != 'number') {
+  if (isNaN(number1) === true || isNaN(number2) === true) {
     confirm('Please enter only numbers');
   }
 
   else {
-  document.querySelector('button').onclick ();
-}
+    var answer = Number(number1) + Number(number2);
+    document.querySelector('.output').innerHTML = answer;
+    console.log(answer);
+  }
+};
